7d5b198db7d756865b9de1874fbac3ee9a2064b1
as <89428507+chiuzon@users.noreply.github.com>
2021-12-11 18:25:41 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,16 @@
 import type { AbstractConnector } from '@web3-react/abstract-connector'
 
+export interface ISolidWeb3 {
+    activate: (connector: AbstractConnector, onError?: ((error: Error) => void) | undefined, throwErrors?: boolean) => Promise<void>,
+    deactivate: () => void,
+    connector: () => AbstractConnector | undefined,
+    library: () => any,
+    chainId: () => number,
+    account: () => string | null | undefined,
+    active: () => boolean,
+    error: () => Error | null | undefined
+}
+
 export interface IWeb3Store {
     connector: AbstractConnector | undefined
     library: any,
